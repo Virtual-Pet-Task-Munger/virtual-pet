1447f2719d77f581caf31e4900395af8cf3d63b0
BenFrancka <benfrancka@gmail.com>
2021-06-01 16:36:13 -0700

    passed three tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,61 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+
+import { findById } from '../functions/utils.js';
+import { getUser, setUser } from '../functions/local-storage-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+//findById tests ////////////////////////////////////////////////////////////////////////////////////
+const someArr = [
+    {
+        id: 1
+    },
+    {
+        id:2
+    }
+];
+test('test findById', (expect) => {
+    
+    const expected = 1;
+    
+    const findPet = findById(someArr, 1);
+    const actual = findPet.id;
+
     expect.equal(actual, expected);
 });
+
+//setUser ////////////////////////////////////////////////////////////////////////////////////////////
+test('setUser should take a user object and put the strigified version of the object into local storage ', (expect) => {
+  
+    const user = {
+        username: 'ben',
+        petname: 'spot'
+    };
+    
+    setUser(user);
+    const actual = {
+        username: 'ben',
+        petname: 'spot'
+    };
+
+    const expected = getUser('ben');
+
+    expect.deepEqual(actual, expected);
+});
+
+//getUser///////////////////////////////////////////////////////////////////////////////////////
+test('getUser should get a stringified user object from local storage and parse it,  return parsed object', (expect) => {
+   
+    const user = {
+        username: 'charles',
+        petname: 'jeep',
+        water: 2
+    };
+    setUser(user);
+    const expected = getUser('charles');
+    
+    expect.deepEqual(user, expected);
+});
+
+
+
