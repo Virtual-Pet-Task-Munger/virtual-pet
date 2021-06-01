74f06397336e9e8b667c06d824821b1be139b87e
Peter <pete225848@gmail.com>
2021-06-01 11:33:11 -0700

    Built pet buttons food, water, love

--- pet/pet.js
+++ pet/pet.js
@@ -1,10 +1,31 @@
-import { logoutUser } from '../functions/local-storage-utils.js';
+import { logoutUser, getCurrentUser, setUser } from '../functions/local-storage-utils.js';
 import { redenderPet } from './render-pet.js';
 
 const logoutButton = document.querySelector('#logout');
+const foodButton = document.querySelector('#food');
+const waterButton = document.querySelector('#water');
+const loveButton = document.querySelector('#love');
 
 redenderPet();
 
 logoutButton.addEventListener('click', () => {
     logoutUser();
+});
+
+foodButton.addEventListener('click', () => {
+    const user = getCurrentUser();
+    user.food++;
+    setUser(user);
+});
+
+waterButton.addEventListener('click', () => {
+    const user = getCurrentUser();
+    user.water++;
+    setUser(user);
+});
+
+loveButton.addEventListener('click', () => {
+    const user = getCurrentUser();
+    user.love++;
+    setUser(user);
 });
